5f242133d4de50dd022ce1b3d6972459d790f68a
キツネ神 <34921200+FriskyFox@users.noreply.github.com>
2018-07-12 16:26:18 -0800

    Beggining work on auto configuration migration

--- lib/local.py
+++ lib/local.py
@@ -4,14 +4,49 @@ import datetime
 import os
 
 # Personal Imports
-from . import constants
+from lib import constants
 
-def make_config():
-    with open('config.ini', 'wt', encoding = 'utf_8_sig') as outfile:
+def make_config(name='config.ini'):
+    with open(name, 'wt', encoding = 'utf_8_sig') as outfile:
         outfile.write(constants.DEFAULT_CONFIG_TEXT)
-        print("[i] New default config file created. Please add tag groups to this file.'")
+        if name == 'config.ini':
+            print("[i] New default config file created. Please add tag groups to this file.'")
+            input("Press ENTER to continue...")
+            raise SystemExit
 
-    raise SystemExit
+def migrate_config():
+    configOld = configparser.ConfigParser()
+    configNew = configparser.ConfigParser()
+    with open('config.ini', 'rt', encoding = 'utf_8_sig') as oldFile:
+        configOld.read_file(oldFile)
+    make_config('configMigration.ini')
+    with open('configMigration.ini', 'wt', encoding = 'utf_8_sig') as newFile:
+        configNew.read_file(newFile)
+
+    #Simple accessor and mutator methods
+    for section in config.sections():
+        if section.lower() == 'other':
+            for option, value in config.items(section):
+                if option.lower() == 'include_md5':
+                        configNew['Other']['include_md5'] = value
+                elif option.lower() == 'organize_by_type':
+                        configNew['Other']['organize_by_type'] = value
+        elif section.lower() == 'defaults':
+            for option, value in config.items(section):
+                if option.lower() in {'days_to_check', 'days'}:
+                    configNew['Defaults']['days'] = value
+                elif option.lower() in {'min_score', 'score'}:
+                    configNew['Defaults']['min_score'] = value
+                elif option.lower() in {'min_favs', 'favs'}:
+                    configNew['Defaults']['min_favs'] = value
+                elif option.lower() in {'ratings', 'rating'}:
+                    configNew['Defaults']['ratings'] = value
+        elif section.lower() == 'blacklist':
+            configNew['Blacklist']['tags'] = value
+        else:
+            configNew[section] = {}
+            for option, value in config.items(section):
+                configNew[section][option] = value
 
 def get_config():
     config = configparser.ConfigParser()
